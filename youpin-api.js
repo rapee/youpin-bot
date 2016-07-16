c0813c67c21a94caf673ea51aa1fb0001df3c930
parnurzeal <parnurzeal@gmail.com>
2016-07-16 20:30:28 +0700

    Add authentication

--- youpin-api.js
+++ youpin-api.js
@@ -1,31 +1,74 @@
-const request = require('request');
+'use strict';
 
-module.exports = (uri) => {
-  return {
-    postPin: function(json, callback) {
-      request(
+const request = require('request');
+const superagent = require('superagent');
+
+const feathers = require('feathers/client');
+const rest = require('feathers-rest/client');
+const hooks = require('feathers-hooks');
+const authentication = require('feathers-authentication/client');
+
+class Api {
+  // initialise api and do authentication
+  constructor(uri, username, password) {
+    this.uri = uri;
+    this.token;
+    this.username = username;
+    this.password = password;
+    const app = feathers()
+      .configure(hooks())
+      .configure(rest(uri).request(request))
+      .configure(authentication());
+    app.authenticate({
+      type: 'local',
+      'email': username,
+      'password': password
+    }).then(result => {
+      this.token = app.get('token');
+      return this;
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
+  postPin(json, callback) {
+    request(
         {
-          uri: uri + '/pins',
+          uri: this.uri + '/pins',
           method: 'POST',
           json: json,
-          headers: {'Content-Type': 'application/json'}
+          headers: {'Content-Type': 'application/json'},
+          auth: {'Bearer': this.token}
         },
         function (error, response, body) {
           if (!error && response.statusCode == 200) {
             callback(body);
           } else {
-            console.error('Unable to post a new pin.');
+            console.error('Unable to upload an image.');
             console.error(response);
             console.error(error);
           }
         }
       );
-    },
+    /*superagent
+      .post(this.uri + '/pins')
+      .set('Authorization', 'Bearer ' + this.token)
+      .send(json)
+      .end(function(error, response) {
+        if (!error && response.statusCode == 200) {
+          callback(response.body);
+        } else {
+          console.error('Unable to post a new pin.');
+          console.error(response);
+          console.error(error);
+        }
+      });*/
+  }
 
-    uploadPhotoFromURL: function(imgLink, callback) {
-      request(
+  uploadPhotoFromURL(imgLink, callback) {
+    request(
         {
-          uri: uri + '/photos/uploadfromurl',
+          uri: this.uri + '/photos/uploadfromurl',
           method: 'POST',
           json: imgLink,
           headers: {'Content-Type': 'application/json'}
@@ -40,6 +83,7 @@ module.exports = (uri) => {
           }
         }
       );
-    }
-  };
+  }
 };
+
+module.exports = Api;
